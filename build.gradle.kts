bbf61a802dc021abd1b0014b1596dfee58e287c7
Weesl <sefasam553498@gmail.com>
2025-08-10 17:51:52 +0300

    update build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,14 @@ plugins {
 
 group = "net.weesli.veldoracore"
 
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://papermc.io/repo/repository/maven-public/")
+    }
+}
+
+
 dependencies{
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
 }
